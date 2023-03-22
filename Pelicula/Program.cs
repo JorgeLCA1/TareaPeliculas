692930aa3dac60740c6999193f2dde76c06de819
JorgeLCA1 <126517251+JorgeLCA1@users.noreply.github.com>
2023-03-22 07:09:45 +0000

    Prueba

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -14,72 +14,109 @@ namespace Pelicula
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
-        public Pelicula(){}
-        public Pelicula(string titulo, Int16 año, string pais, string director)
-        {
-            this.titulo = titulo;
-            this.año = año;
-            this.pais = pais;
-            this.director = director;
-        }
-
-     
-        //Métodos
-        public string GetTitulo()
-        {
-            return titulo;
-        }
-        public void SetTitulo(string titulo)
-        {
-            this.titulo = titulo;
-        }
         public Int16 GetAño()
         {
             return año;
         }
-        public void SetAño(Int16 año)
+        public void SetAño(Int16 añoo)
         {
-            this.año = año;
+            año = añoo;
         }
-        public string GetPais()
+
+        public string GetTitulo()
         {
-            return pais;
+            return titulo;
         }
-        public void SetPais(string pais)
+        public void SetTitulo(string tituloo)
         {
-            this.pais = pais;
+            titulo = tituloo;
         }
-        public string GetDirector()
+        public string Titulo{
+            get{return titulo;}
+            set{titulo = value;}
+        }
+        public Int16 Año{
+            get{return año;}
+            set{año = value;}
+        }
+        public Pelicula(){}
+        public Pelicula(string Titulo, Int16 Año)
         {
-            return director;
+            this.Titulo = Titulo;
+            this.Año = Año;
+            
         }
-        public void SetDirector(string director)
-        {
-            this.director = director;
-        }
+        
+     
+        //Métodos
+       
 
 
         public void Imprime()
         {
             Console.WriteLine($"{titulo} ({año})");
-
+            
         }
-
-
-    }
+       public void ImprimerActores(){
+        foreach (Actor añoo in actores)
+        {
+            Console.WriteLine(añoo);
+        }
+       }
+         public void AgregarActor(Actor actor){
+              actores.Add(actor);
+         }
 
     public class Actor
     {
         //Propiedades
+    private string Nombre;
+    private Int16 AñoActor;
 
         //Constructores
+    public Actor(string Nombre, Int16 AñoActor){
+        this.Nombre = Nombre;
+        this.AñoActor = AñoActor;
+    }
 
 
         //Métodos 
+
+public override string ToString()
+        {
+            return $"{Nombre} ({AñoActor})";
+        }
+        public Int16 getAñoActor()
+        {
+            return AñoActor;
+        }
+        public void setAñoActor(Int16 añoo)
+        {
+            AñoActor = añoo;
+        }
+        public string getNombre()
+        {
+            return Nombre;
+        }
+        public void setNombre(string nom)
+        {
+            Nombre = nom;
+        }
+        public string nombre{
+            get{return Nombre;}
+            set{Nombre = value;}
+        }
+        public Int16 añoActor{
+            get{return AñoActor;}
+            set{AñoActor = value;}
+        }
+        
+        
         public void Imprime()
         {
-            //Console.WriteLine($"{nombre} ({año})");
+            Console.WriteLine($"{Nombre} ({AñoActor})");
         }
+
     }
 
     // Puedes probar tu código en Main() pero lo importante
@@ -91,8 +128,18 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
-        Pelicula pelicula1 = new Pelicula();
-        pelicula1.SetTitulo("Forest Gump");
+        Pelicula pelicula1 = new Pelicula("Forest Gump", 1994);
+        pelicula1.AgregarActor(new Actor("Tom Hanks", 1980));
+        pelicula1.AgregarActor(new Actor("Robin Wright", 1986));
+        pelicula1.Imprime();
+        pelicula1.ImprimerActores();
+        
+        
+        
+        
+        
+        
+        /* pelicula1.SetTitulo("Forest Gump");
         pelicula1.SetAño(1994);
         pelicula1.SetPais("Estados Unidos");
         pelicula1.SetDirector("Robert Zemeckis");
@@ -105,10 +152,23 @@ namespace Pelicula
         pelicula2.SetDirector("Peter Jackson");
         Console.WriteLine("{0} - ({1})", pelicula2.GetTitulo(), pelicula2.GetAño());
 
+        pelicula1.AgregarActor(new Actor("Tom Hanks", 1980));
+        pelicula1.AgregarActor(new Actor("Robin Wright", 1986));
+        pelicula1.ImprimerActores();
+
+        //Lista peliculas
+        List<Pelicula> peliculas = new List<Pelicula>();
+        peliculas.Add(new Pelicula("Forest Gump", 1994, "Estados Unidos", "Robert Zemeckis"));
+        peliculas.Add(new Pelicula("El señor de los anillos: el retorno del rey", 2003, "Nueva Zelanda", "Peter Jackson"));
+        peliculas.Add(new Pelicula("Braveheart", 1995, "Estados Unidos", "Mel Gibson"));
+        peliculas.Add(new Pelicula("Titanic", 1997, "Estados Unidos", "James Cameron"));
+        peliculas.Add(new Pelicula("Rocky", 1976, "Estados Unidos", "John G. Avildsen"));
         
-        
-        
+        foreach(Pelicula pelicula in peliculas)
+        {
+            Console.WriteLine($"{pelicula.GetTitulo()} ({pelicula.GetAño()})"); 
+        } */
         
         }
     }
-}
+}}
