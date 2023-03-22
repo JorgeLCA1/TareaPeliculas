9087f06927bbec6d5c1c68723225d0ebe917ac57
JorgeLCA1 <126517251+JorgeLCA1@users.noreply.github.com>
2023-03-22 02:43:32 +0000

    Primera versión

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -49,7 +49,24 @@ namespace Pelicula
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+        Pelicula pelicula1 = new Pelicula();
+        pelicula1.titulo="Forest Gump";
+        pelicula1.año=1994;
+        pelicula1.pais="Estados Unidos";
+        pelicula1.director="Robert Zemeckis";
+
+        Pelicula pelicula2= new Pelicula();
+        pelicula2.titulo="El señor de los anillos: el retorno del rey";
+        pelicula2.año=2003;
+        pelicula2.pais="Nueva Zelanda";
+        pelicula2.director="Peter Jackson";
+
+        
+        Console.WriteLine("Peliculas ganadoras de Oscar a mejor película");
+        console.WriteLine("============================================");
+        console.WriteLine("- "+pelicula1.titulo+" ("+pelicula1.año+") - "+pelicula1.pais+" - "+pelicula1.director);
+        Console.WriteLine("- "+pelicula2.titulo+" ("+pelicula2.año+") - "+pelicula2.pais+" - "+pelicula2.director);
+        
         }
     }
 }
