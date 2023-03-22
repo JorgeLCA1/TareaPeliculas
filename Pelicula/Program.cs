58019e0f1df5cc113a529a6409a64485961f7056
JorgeLCA1 <126517251+JorgeLCA1@users.noreply.github.com>
2023-03-22 03:54:19 +0000

    Agregado metodos get set

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -7,19 +7,52 @@ namespace Pelicula
     public class Pelicula
     {
         //Campos
-        public string titulo;
-        public Int16 año;
-        public string pais;
-        public string director;
+        private string titulo;
+        private Int16 año;
+        private string pais;
+        private string director;
         private List<Actor> actores = new List<Actor>();
 
         //Constructores
      
         //Métodos
-     
+        public string GetTitulo()
+        {
+            return titulo;
+        }
+        public void SetTitulo(string titulo)
+        {
+            this.titulo = titulo;
+        }
+        public Int16 GetAño()
+        {
+            return año;
+        }
+        public void SetAño(Int16 año)
+        {
+            this.año = año;
+        }
+        public string GetPais()
+        {
+            return pais;
+        }
+        public void SetPais(string pais)
+        {
+            this.pais = pais;
+        }
+        public string GetDirector()
+        {
+            return director;
+        }
+        public void SetDirector(string director)
+        {
+            this.director = director;
+        }
+
+
         public void Imprime()
         {
-     //       Console.WriteLine($"{titulo} ({año})");
+        Console.WriteLine($"{titulo} ({año})");
 
         }
 
@@ -50,22 +83,22 @@ namespace Pelicula
         static void Main(string[] args)
         {
         Pelicula pelicula1 = new Pelicula();
-        pelicula1.titulo="Forest Gump";
-        pelicula1.año=1994;
-        pelicula1.pais="Estados Unidos";
-        pelicula1.director="Robert Zemeckis";
+        pelicula1.SetTitulo("Forest Gump");
+        pelicula1.SetAño(1994);
+        pelicula1.SetPais("Estados Unidos");
+        pelicula1.SetDirector("Robert Zemeckis");
+        Console.WriteLine("{0} - ({1})", pelicula1.GetTitulo(), pelicula1.GetAño());
 
         Pelicula pelicula2= new Pelicula();
-        pelicula2.titulo="El señor de los anillos: el retorno del rey";
-        pelicula2.año=2003;
-        pelicula2.pais="Nueva Zelanda";
-        pelicula2.director="Peter Jackson";
+        pelicula2.SetTitulo("El señor de los anillos: el retorno del rey");
+        pelicula2.SetAño(2003);
+        pelicula2.SetPais("Nueva Zelanda");
+        pelicula2.SetDirector("Peter Jackson");
+        Console.WriteLine("{0} - ({1})", pelicula2.GetTitulo(), pelicula2.GetAño());
 
         
-        Console.WriteLine("Peliculas ganadoras de Oscar a mejor película");
-        console.WriteLine("============================================");
-        console.WriteLine("- "+pelicula1.titulo+" ("+pelicula1.año+") - "+pelicula1.pais+" - "+pelicula1.director);
-        Console.WriteLine("- "+pelicula2.titulo+" ("+pelicula2.año+") - "+pelicula2.pais+" - "+pelicula2.director);
+        
+        
         
         }
     }
